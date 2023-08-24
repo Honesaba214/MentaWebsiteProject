3dcab43e550807e8eca57f38bd2cfe6f6af83508
kazuya180 <hone214.oe1@gmail.com>
2023-08-24 21:21:03 +0900

    no message

---
+++ src/main/java/com/example/demo/model/user.java
@@ -0,0 +1,84 @@
+package com.example.demo.model;
+
+/**
+ * 会員クラス
+ */
+public class user {
+
+	
+	/** 
+	 * ユーザーNo
+	 */
+	private int userNumber;
+	
+	/**
+	 * ユーザー名
+	 */
+	private String userName;
+	
+	/**
+	 * ユーザー住所
+	 */
+	private String userAddress;
+	
+	/**
+	 * ユーザーメールアドレス
+	 */
+	private String userEmailAddress;
+	
+	
+	
+	public int getUserNumber() {
+		return userNumber;
+	}
+
+
+
+	public void setUserNumber(int userNumber) {
+		this.userNumber = userNumber;
+	}
+
+
+
+	public String getUserName() {
+		return userName;
+	}
+
+
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+
+
+	public String getUserAddress() {
+		return userAddress;
+	}
+
+
+
+	public void setUserAddress(String userAddress) {
+		this.userAddress = userAddress;
+	}
+
+
+
+	public String getUserEmailAddress() {
+		return userEmailAddress;
+	}
+
+
+
+	public void setUserEmailAddress(String userEmailAddress) {
+		this.userEmailAddress = userEmailAddress;
+	}
+
+
+
+	{
+		
+	}
+	
+	
+}
