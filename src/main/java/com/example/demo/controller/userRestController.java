ec5ef37454df01193f1cf8c455b7d8b36000dd9c
kazuya180 <hone214.oe1@gmail.com>
2023-08-27 15:20:11 +0900

    TestPush20230827

---
+++ src/main/java/com/example/demo/controller/userRestController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api")
+public class userRestController {
+
+	@GetMapping("/user")
+	public String getUser() {
+		return "OK";
+	}
+	
+	@PostMapping()
+	
+	
+}
